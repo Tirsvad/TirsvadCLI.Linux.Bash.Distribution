3f8528db534aac36789b453260f43e5012c4d30c
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-03-01 17:50:02 +0800

    Improved test output

--- src/Test/test_Distribution.sh
+++ src/Test/test_Distribution.sh
@@ -3,22 +3,34 @@
 ## @file test_Distribution.sh
 
 # Source the distribution script
-. ../Distribution/Distribution.sh
+. ../Distribution/Run.sh
+
+failed=0
 
 # Check if the output variables are set
-if [[ -z "$TCLI_LINUX_DISTRIBUTION_ID" ]]; then
-    echo "Test failed: Distribution ID is not set."
-    exit 1
+if [[ -z "$TCLI_LINUX_BASH_DISTRIBUTION_ID" ]]; then
+    echo "Test failed: Get distribution ID."
+    ((failed++))
+    else
+    echo "Passed: Get distribution ID."
 fi
 
-if [[ -z "$TCLI_LINUX_DISTRIBUTION_RELEASE" ]]; then
-    echo "Test failed: Distribution release is not set."
-    exit 1
+if [[ -z "$TCLI_LINUX_BASH_DISTRIBUTION_RELEASE" ]]; then
+    echo "Test failed: Get distribution release."
+    ((failed++))
+    else
+    echo "Passed: Get distribution release."
 fi
 
-if [[ -z "$TCLI_LINUX_DISTRIBUTION_ARCH" ]]; then
-    echo "Test failed: Distribution architecture is not set."
-    exit 1
+if [[ -z "$TCLI_LINUX_BASH_DISTRIBUTION_ARCH" ]]; then
+    echo "Test failed: Get distribution architecture."
+    ((failed++))
+else
+    echo "Passed: Get distribution architecture."
 fi
 
+if [[ "$failed" != 0 ]]; then
+    echo "$failed tests failed."
+    exit 1
+fi
 echo "All tests passed!"
